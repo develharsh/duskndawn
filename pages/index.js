c57b34c2899d72336088eb57db0f5e89ae0ead09
Harshvardhan Singh <51012630+cpbhai@users.noreply.github.com>
2021-09-02 19:46:42 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,22 @@
 import Link from 'next/link'
 import Head from 'next/head'
-
+import { DataContext } from '../store/GlobalState'
+import { useContext} from 'react'
 export default function Home(props) {
+  const { state, dispatch } = useContext(DataContext);
+  const handleView=(e)=>{
+    dispatch({ type: 'NOTIFY', payload: { loading: true } })
+    setTimeout(function(){  dispatch({ type: 'NOTIFY', payload: { loading: false } }); }, 3000);
+  }
   return (
     <div>
       <Head>
         <title>Dusk&apos;n Dawn | Home</title>
       </Head>
       <img src='/banner1.png' width="100%" height="100%" alt="..." />
-      <h1 className="center-align homeBrand">Txt for Testing.........</h1>
+      <h1 className="center-align homeBrand">Just for Testing.........</h1>
       <div className="center-align homeLinkShow">
-        <Link href="/show"><a className="btn-large">View Products</a></Link>
+        <Link href="/show"><a className="btn-large" onClick={(e)=>{handleView(e)}}>View Products</a></Link>
       </div>
     </div>
   )
